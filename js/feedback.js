ad6782fb766375cac2229b1778ac268454867cd5
TarunSaini063 <tsaini063@outlook.com>
2021-04-19 14:31:06 +0530

    Fix feedback slider

--- js/feedback.js
+++ js/feedback.js
@@ -1,41 +1,25 @@
-var galleryThumbs = new Swiper('.gallery-thumbs', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: '2',
-    // coverflowEffect: {
-    //   rotate: 50,
-    //   stretch: 0,
-    //   depth: 100,
-    //   modifier: 1,
-    //   slideShadows : true,
-    // },
-
-    coverflowEffect: {
-        rotate: 0,
-        stretch: 0,
-        depth: 50,
-        modifier: 6,
-        slideShadows : false,
-    },
-
-});
-
-
-var galleryTop = new Swiper('.swiper-container.testimonial', {
-    speed: 400,
-    spaceBetween: 50,
-    autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-    },
-    direction: 'vertical',
-    pagination: {
-        clickable: true,
-        el: '.swiper-pagination',
-        type: 'bullets',
-    },
-    thumbs: {
-        swiper: galleryThumbs
-    }
+jQuery(document).ready(function($) {
+    "use strict";
+    //  TESTIMONIALS CAROUSEL HOOK
+    $('#customers-testimonials').owlCarousel({
+        loop: true,
+        center: true,
+        items: 3,
+        margin: 0,
+        autoplay: true,
+        dots:true,
+        autoplayTimeout: 8500,
+        smartSpeed: 450,
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            1170: {
+                items: 3
+            }
+        }
+    });
 });
